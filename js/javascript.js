0315f1b79e8e32b6469db7b62fe3b6410e59e26e
Michael Imhoff <112586751+mwimh@users.noreply.github.com>
2026-02-01 14:13:57 -0600

    Selections Working
    RPS Selections

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const compChoice = Math.floor(Math.random() * 3);
+    const playerChoice = prompt("Rock, Scissors, or Paper?", "Rock");
+    let comp;
+    let player;
+
+    if (compChoice == 0) {
+        comp = "Rock";
+    } else if (compChoice == 1) {
+        comp = "Scissors";
+    } else {
+        comp = "Paper";
+    }
+
+    if (playerChoice == "Rock") {
+        player = 0;
+    } else if (playerChoice == "Scissors") {
+        player = 1;
+    } else if (playerChoice == "Paper") {
+        player = 2;
+    } else {
+        console.log("Please choose Rock, Paper, or Scissors");
+    };
+
+console.log(player);
+
+};
+
+getComputerChoice();
