2b2a7ff4eafc11710734b3e902d370430b743619
Michael Imhoff <112586751+mwimh@users.noreply.github.com>
2026-02-25 16:11:45 -0600

    RPS Game Working!
    It works! ALL IN FUNCTIONS!

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,6 @@
 function getComputerChoice() {
     const compChoice = Math.floor(Math.random() * 3);
-    // const playerChoice = prompt("Rock, Scissors, or Paper?", "Rock");
-    const playerChoice = "Rock";
     let comp;
-    let player;
 
     if (compChoice == 0) {
         comp = "Rock";
@@ -13,23 +10,47 @@ function getComputerChoice() {
         comp = "Paper";
     }
 
-    if (playerChoice == "Rock") {
-        player = 0;
-    } else if (playerChoice == "Scissors") {
-        player = 1;
-    } else if (playerChoice == "Paper") {
-        player = 2;
+    console.log("Computer: ", comp);
+
+    return (compChoice);
+}
+
+function getPlayerChoice() {
+    const player = prompt("Rock, Scissors, or Paper?", "Rock");
+    let playerChoice;
+
+    if (player == "Rock") {
+        playerChoice = 0;
+    } else if (player == "Scissors") {
+        playerChoice = 1;
+    } else if (player == "Paper") {
+        playerChoice = 2;
     } else {
-        console.log("Please choose Rock, Paper, or Scissors");
+        console.log("Please choose (R)ock, (P)aper, or (S)cissors");
     };
 
-    if (compChoice === player) {
+    console.log("Player: ", player);
+
+    return (playerChoice);
+
+}
+
+function runGame() {
+    const C = getComputerChoice();
+    const P = getPlayerChoice();
+
+    if (C == P) {
         console.log("Tie!")
+    } else if (C == 2 && P == 0) {
+        console.log("Computer Wins")
+    } else if (C == 0 && P == 2) {
+        console.log("Player Wins")
+    } else if (C < P) {
+        console.log("Computer Wins")
+    } else {
+        console.log("Player Wins")
     }
 
-console.log("Computer Choice: ", comp);
-console.log("Player Choice: ", playerChoice);
+}
 
-};
-
-getComputerChoice();
+runGame();
